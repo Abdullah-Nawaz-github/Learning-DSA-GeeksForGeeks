cffc5d8e1b5169080fea8b4b930ec63550820c49
abdullah <abdullah@localhost>
2023-03-16 15:18:28 +0300

    Add leftRotate method

--- DSA-GeeksForGeeks/src/Arrays/ArrayMethods.java
+++ DSA-GeeksForGeeks/src/Arrays/ArrayMethods.java
@@ -6,7 +6,7 @@ public class ArrayMethods {
 
 //	Given an array (or string), the task is to reverse the array/string.
 	public static int[] reverse(int[] arr) {
-		// Method 1:
+		// Method 1 (For Loop):
 
 //		int[] result = new int[arr.length];
 //		int index = 0;
@@ -15,7 +15,7 @@ public class ArrayMethods {
 //		}
 //		return result;
 
-		// Method 2:
+		// Method 2 (While Loop):
 
 		int start = 0;
 		int end = arr.length - 1;
@@ -29,9 +29,45 @@ public class ArrayMethods {
 		return arr;
 	}
 
+//	Given an array of integers arr[] of size N and an integer, the task is to rotate the array elements to the left by d positions.
+	public static int[] leftRotate(int[] arr, int d) {
+
+//		First Try (temp array):
+
+//		int[] result = new int[arr.length];
+//		int count = 0;
+//		for (int i = 0; i < arr.length; i++) {
+//			int x = i + d;
+//			if (x >= arr.length) {
+//				x = x - i - d + count;
+//				count++;
+//			}
+//			result[i] = arr[x];
+//		}
+//		return result;
+
+//		Method 1 (temp array):
+
+		// creating result array
+		int[] result = new int[arr.length];
+		// keeping track of index of result array
+		int k = 0;
+
+		// adding elements starting from d to the front of result array
+		for (int i = d; i < result.length; i++) {
+			result[k++] = arr[i];
+		}
+
+		// adding elements from the front of arr to back of result array
+		for (int i = 0; i < d; i++) {
+			result[k++] = arr[i];
+		}
+		return result;
+	}
+
 	public static void main(String[] args) {
-		int[] arr1 = { 1, 2, 3, 4, 5, 6 };
-		System.out.println(Arrays.toString(ArrayMethods.reverse(arr1)));
+		int[] arr1 = { 3, 4, 5, 6, 7, 1, 2 };
+		System.out.println(Arrays.toString(ArrayMethods.leftRotate(arr1, 2)));
 	}
 
 }
