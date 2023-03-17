02f77a8a23b5e223b5d64dbdd3ef45ccc313e8d3
abdullah <abdullah@localhost>
2023-03-17 11:37:40 +0300

    Add binarySort method

--- DSA-GeeksForGeeks/src/Arrays/ArrayMethods.java
+++ DSA-GeeksForGeeks/src/Arrays/ArrayMethods.java
@@ -229,8 +229,21 @@ public class ArrayMethods {
 		return n;
 	}
 
+	// Binary Sort
+	public static void binarySort(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++)
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+		}
+	}
+
 	public static void main(String[] args) {
 		int[] arr1 = { 1, 2, 3, 4, 5, 0 }; // n=4, capacity=5
+		System.out.println(Arrays.toString(arr1));
 
 	}
 
